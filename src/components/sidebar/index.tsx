2eee82b81307ffb7b8cc8b5f6577250b0e94a4e9
人间有味是清欢 <39555417+gjycn@users.noreply.github.com>
2023-11-18 00:40:11 +0800

    Initialize renovate/all-minor-patch

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,316 @@
+import { NIcon, NLayoutContent } from 'naive-ui'
+import Icon from 'naive-ui/es/_internal/icon/src/Icon'
+import type { PropType } from 'vue'
+import { computed, defineComponent, onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { onClickOutside } from '@vueuse/core'
+
+import { WEB_URL } from '~/constants/env'
+import { RouteName } from '~/router/name'
+import { UIStore } from '~/stores/ui'
+import { RESTManager } from '~/utils'
+
+import { configs } from '../../configs'
+import { useStoreRef } from '../../hooks/use-store-ref'
+import { useUserStore } from '../../stores/user'
+import type { MenuModel } from '../../utils/build-menus'
+import { buildMenus } from '../../utils/build-menus'
+import { Avatar } from '../avatar'
+import { useSidebarStatusInjection } from './hooks'
+import styles from './index.module.css'
+
+export const Sidebar = defineComponent({
+  name: 'SideBar',
+  props: {
+    collapse: {
+      type: Boolean,
+      required: true,
+    },
+    width: {
+      type: Number,
+      required: true,
+    },
+    onCollapseChange: {
+      type: Function as PropType<{ (status: boolean): void }>,
+      required: true,
+    },
+  },
+  setup(props) {
+    const router = useRouter()
+    const { user } = useUserStore()
+    const route = computed(() => router.currentRoute.value)
+    const menus = ref<MenuModel[]>([])
+    onMounted(() => {
+      // @ts-expect-error
+      menus.value = buildMenus(router.getRoutes())
+    })
+
+    const indexRef = ref(0)
+
+    function updateIndex(nextIndex: number) {
+      if (nextIndex === indexRef.value) {
+        indexRef.value = -1
+        return
+      }
+      indexRef.value = nextIndex
+    }
+
+    function handleRoute(item: MenuModel, nextIndex?: number) {
+      if (item.subItems?.length) {
+        return
+      }
+
+      router.push({
+        path: item.fullPath,
+        query: item.query,
+      })
+      if (typeof nextIndex === 'number') {
+        updateIndex(nextIndex)
+      }
+    }
+
+    const title = configs.title
+    const sidebarRef = ref<HTMLDivElement>()
+    const uiStore = useStoreRef(UIStore)
+    onClickOutside(sidebarRef, () => {
+      const v = uiStore.viewport
+      const isM = v.value.pad || v.value.mobile
+      if (isM) {
+        props.onCollapseChange(true)
+      }
+    })
+    const { isDark, toggleDark } = useStoreRef(UIStore)
+
+    const { onTransitionEnd, statusRef } = useSidebarStatusInjection(
+      () => props.collapse,
+    )
+
+    return () => {
+      const isPhone = uiStore.viewport.value.mobile
+      return (
+        <div
+          class={[
+            styles['root'],
+            props.collapse ? styles['collapse'] : null,
+
+            styles[statusRef.value],
+          ]}
+          style={{
+            width: !props.collapse && props.width ? `${props.width}px` : '',
+          }}
+          onTransitionend={onTransitionEnd}
+          ref={sidebarRef}
+        >
+          <div class={styles['sidebar']}>
+            <div
+              class={
+                'flex-shrink-0 relative font-medium text-center text-2xl h-20'
+              }
+            >
+              <button
+                class={styles['toggle-color-btn']}
+                onClick={() => void toggleDark()}
+              >
+                {!isDark.value ? (
+                  <i class={'icon-[ph--sun-bold]'} />
+                ) : (
+                  <i class={'icon-[ph--moon-bold]'} />
+                )}
+              </button>
+              <h1 class={styles['header-title']}>{title}</h1>
+              <button
+                class={styles['collapse-button']}
+                onClick={() => {
+                  props.onCollapseChange(!props.collapse)
+                }}
+              >
+                <i class={[styles['collapse-icon']]}>
+                  <svg width="1em" height="1em" viewBox="0 0 24 24">
+                    <path
+                      d="M18.41 16.59L13.82 12l4.59-4.59L17 6l-6 6l6 6l1.41-1.41M6 6h2v12H6V6z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </i>
+              </button>
+            </div>
+
+            <NLayoutContent class={styles['menu']} nativeScrollbar={false}>
+              <div class={styles['items']}>
+                {menus.value.map((item, index) => {
+                  return (
+                    <div
+                      class={[
+                        'py-2',
+                        route.value.fullPath === item.fullPath ||
+                        route.value.fullPath.startsWith(item.fullPath)
+                          ? styles['active']
+                          : '',
+
+                        styles['item'],
+                      ]}
+                      data-path={item.fullPath}
+                    >
+                      <MenuItem
+                        className={!isPhone ? '!py-2' : ''}
+                        title={item.title}
+                        onClick={() =>
+                          item.subItems?.length
+                            ? updateIndex(index)
+                            : handleRoute(item, index)
+                        }
+                        collapse={props.collapse}
+                      >
+                        {{
+                          icon() {
+                            return item.icon
+                          },
+                        }}
+                      </MenuItem>
+
+                      {item.subItems && (
+                        <ul
+                          class={[
+                            'overflow-hidden',
+                            item.subItems.length ? styles['has-child'] : '',
+                            indexRef.value === index ? styles['expand'] : '',
+                          ]}
+                          style={{
+                            maxHeight:
+                              indexRef.value === index
+                                ? `${item.subItems.length * 3.5}rem`
+                                : '0',
+                          }}
+                        >
+                          {item.subItems.map((child) => {
+                            return (
+                              <li
+                                key={child.path}
+                                data-path={child.fullPath}
+                                class={[
+                                  route.value.fullPath === child.fullPath ||
+                                  route.value.fullPath.startsWith(
+                                    child.fullPath,
+                                  )
+                                    ? styles['active']
+                                    : '',
+                                  styles['item'],
+                                ]}
+                              >
+                                <MenuItem
+                                  collapse={props.collapse}
+                                  title={child.title}
+                                  onClick={() => handleRoute(child)}
+                                  className={'py-4'}
+                                >
+                                  {{
+                                    icon() {
+                                      return child.icon
+                                    },
+                                  }}
+                                </MenuItem>
+                              </li>
+                            )
+                          })}
+                        </ul>
+                      )}
+                    </div>
+                  )
+                })}
+              </div>
+            </NLayoutContent>
+
+            <button
+              class={styles['sidebar-footer']}
+              onClick={() => {
+                window.open(WEB_URL)
+              }}
+            >
+              <LogoutAvatarButton />
+
+              <span class={styles['sidebar-username']}>{user?.name}</span>
+            </button>
+          </div>
+        </div>
+      )
+    }
+  },
+})
+
+const MenuItem = defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    onClick: {
+      type: Function as PropType<() => any>,
+      required: true,
+    },
+    collapse: {
+      type: Boolean,
+      required: true,
+    },
+    className: {
+      type: String,
+    },
+  },
+
+  setup(props, { slots }) {
+    return () => (
+      <button
+        onClick={props.onClick}
+        class={['flex w-full items-center py-4', props.className]}
+      >
+        <span
+          class={[
+            'flex justify-center items-center basis-12 transition-all duration-300 ease-in-out',
+            props.collapse ? 'basis-[var(--w)]' : '',
+          ]}
+        >
+          {slots.icon!()}
+        </span>
+        <span class={styles['item-title']}>{props.title}</span>
+      </button>
+    )
+  },
+})
+
+const LogoutAvatarButton = defineComponent({
+  setup() {
+    const { user } = useUserStore()
+    const router = useRouter()
+    const handleLogout = async (e: MouseEvent) => {
+      e.stopPropagation()
+      await RESTManager.api.user.logout.post({})
+      router.push({
+        name: RouteName.Login,
+      })
+    }
+
+    return () => {
+      const avatar = user?.avatar
+
+      return (
+        <div
+          class={'relative h-[35px] w-[35px]'}
+          onClick={handleLogout}
+          role="button"
+        >
+          <Avatar src={avatar} size={35} class="absolute inset-0 z-1" />
+          <div
+            class={[
+              'absolute z-2 inset-0 flex items-center justify-center bg-dark-200 bg-opacity-80 rounded-full hover:opacity-50 opacity-0 transition-opacity',
+              'text-xl',
+            ]}
+          >
+            <NIcon>
+              <i class={''} />
+            </NIcon>
+          </div>
+        </div>
+      )
+    }
+  },
+})
