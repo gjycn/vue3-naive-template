1e47db1fef750e8f1f152925c637db273297c212
人间有味是清欢 <39555417+gjycn@users.noreply.github.com>
2023-11-18 00:40:09 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import type { GlobalThemeOverrides } from 'naive-ui'
+import {
+  darkTheme,
+  dateZhCN,
+  NConfigProvider,
+  NDialogProvider,
+  NMessageProvider,
+  NNotificationProvider,
+  useMessage,
+  useNotification,
+  zhCN,
+} from 'naive-ui'
+import { defineComponent, onMounted } from 'vue'
+import { RouterView } from 'vue-router'
+import { useStoreRef } from './hooks/use-store-ref'
+import { useUIStore } from './stores/ui'
+import { useUserStore } from './stores/user'
+
+const Root = defineComponent({
+  name: 'Home',
+
+  setup() {
+    const { fetchUser } = useStoreRef(useUserStore)
+
+    onMounted(() => {
+      window.message = useMessage()
+      window.notification = useNotification()
+
+      fetchUser()
+    })
+
+    return () => {
+      return <RouterView />
+    }
+  },
+})
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: 'var(--theme-color)',
+    primaryColorHover: 'var(--theme-hover-color)',
+    primaryColorPressed: 'var(--theme-active-color)',
+    primaryColorSuppl: 'var(--theme-suppl-color)',
+  },
+}
+
+const App = defineComponent({
+  setup() {
+    const { isDark, naiveUIDark } = useStoreRef(useUIStore)
+    return () => (
+      <NConfigProvider
+        locale={zhCN}
+        dateLocale={dateZhCN}
+        themeOverrides={themeOverrides}
+        theme={naiveUIDark.value ? darkTheme : isDark.value ? darkTheme : null}
+      >
+        <NNotificationProvider>
+          <NMessageProvider>
+            <NDialogProvider>
+              <Root />
+            </NDialogProvider>
+          </NMessageProvider>
+        </NNotificationProvider>
+      </NConfigProvider>
+    )
+  },
+})
+
+// eslint-disable-next-line import/no-default-export
+export default App
---
+++ src/layouts/sidebar/index.tsx
@@ -0,0 +1,83 @@
+import { NLayoutContent } from 'naive-ui'
+import { RESTManager } from 'utils'
+import type { CSSProperties } from 'vue'
+import { computed, defineComponent, watchEffect } from 'vue'
+import { RouterLink } from 'vue-router'
+
+import $RouterView from '~/layouts/router-view'
+
+import { Sidebar } from '../../components/sidebar'
+import { useStoreRef } from '../../hooks/use-store-ref'
+import { UIStore } from '../../stores/ui'
+import styles from './index.module.css'
+
+export const SidebarLayout = defineComponent({
+  name: 'SidebarLayout',
+
+  setup() {
+    const ui = useStoreRef(UIStore)
+
+    const { meta, b } = useMagicKeys()
+    watchEffect(() => {
+      if (meta.value && b.value) {
+        collapse.value = !collapse.value
+      }
+    })
+
+    const isInApiDebugMode =
+      localStorage.getItem('__api') ||
+      localStorage.getItem('__gateway') ||
+      new URLSearchParams(location.search).get('__api') ||
+      new URLSearchParams(location.search).get('__gateway')
+
+    const collapse = ui.sidebarCollapse
+    const isLaptop = computed(
+      () => ui.viewport.value.mobile || ui.viewport.value.pad,
+    )
+    watchEffect(() => {
+      collapse.value = isLaptop.value ? true : false
+    })
+
+    const sidebarWidth = ui.sidebarWidth
+
+    return () => (
+      <div class={styles['root']}>
+        {isInApiDebugMode && (
+          <div
+            class="h-[40px] fixed top-0 left-0 right-0 bg-dark-800 z-2 text-gray-400 flex items-center transition-all duration-500 whitespace-pre"
+            style={{
+              paddingLeft: !collapse.value ? '270px' : '80px',
+            }}
+          >
+            You are in customizing the API endpoint mode, please check:{' '}
+            <RouterLink to={'/setup-api'}>setup-api</RouterLink>. Endpoint:{' '}
+            {RESTManager.endpoint}
+          </div>
+        )}
+        <Sidebar
+          collapse={collapse.value}
+          width={sidebarWidth.value}
+          onCollapseChange={(s) => {
+            collapse.value = s
+          }}
+        />
+
+        <NLayoutContent
+          embedded
+          nativeScrollbar={false}
+          class={styles['content']}
+          style={
+            {
+              left: !collapse.value ? `${sidebarWidth.value}px` : '50px',
+              pointerEvents:
+                isLaptop.value && !collapse.value ? 'none' : 'auto',
+              top: isInApiDebugMode && '40px',
+            } as CSSProperties
+          }
+        >
+          <$RouterView />
+        </NLayoutContent>
+      </div>
+    )
+  },
+})
